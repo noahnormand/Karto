429fac9fd8ffcc0cbb3218fd95fb397c09182878
Noah Normand <noah.normand@student.junia.com>
2026-06-26 02:23:21 +0200

    Fix: add Tailwind CDN to CSP script-src (was blocking all styling)

--- server.js
+++ server.js
@@ -85,10 +85,10 @@ app.use((_req, res, next) => {
     res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains')
     res.setHeader('Content-Security-Policy', [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' https://static-cdn.jtvnw.net",
-      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+      "script-src 'self' 'unsafe-inline' https://cdn.tailwindcss.com https://static-cdn.jtvnw.net",
+      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.tailwindcss.com",
       "font-src 'self' https://fonts.gstatic.com",
-      "img-src 'self' data: https://res.cloudinary.com https://static-cdn.jtvnw.net https://*.twitch.tv",
+      "img-src 'self' data: https://res.cloudinary.com https://static-cdn.jtvnw.net https://*.twitch.tv https://images.pexels.com",
       "connect-src 'self' https://api.twitch.tv https://id.twitch.tv",
       "frame-ancestors 'none'"
     ].join('; '))
